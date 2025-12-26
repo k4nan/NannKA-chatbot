6f3f91baa412833261221f9e5dd36700f2166b73
Hanan Eka <lillnann92@gmail.com>
2025-12-26 16:02:08 +0700

    Upload Final NannKA Chatbot

--- cekmodel.py
+++ cekmodel.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 
 # Ganti TULISAN_INI dengan API Key asli Anda
-API_KEY = "AIzaSyDxsfwhQTiy8MRYCtBzan1VZwT3HcGXwMo" 
+API_KEY = "isi_API_anda_dengan_16_karakter" 
 
 genai.configure(api_key=API_KEY)
 
@@ -13,6 +13,6 @@ try:
 except Exception as e:
     print(f"Error: {e}")
     
-MY_API_KEY = "AIzaSyDxsfwhQTiy8MRYCtBzan1VZwT3HcGXwMo"
-EMAIL_SENDER = "lillnann92@gmail.com" # Email Bot (Pengirim)
-EMAIL_PASSWORD = "vkufjfoeipiolspr" # App Password Gmail Bot
+MY_API_KEY = "isi_API_anda_dengan_16_karakter"
+EMAIL_SENDER = "email_anda" # Email Bot (Pengirim)
+EMAIL_PASSWORD = "sandi_email_anda" # App Password Gmail Bot
